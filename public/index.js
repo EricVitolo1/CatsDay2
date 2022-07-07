a0be86339158eb753f2c4565ba55a3d96826a12c
Eric <eric@DESKTOP-HHJOMI5.localdomain>
2022-07-07 13:35:40 -0700

    hey it works

--- public/index.js
+++ public/index.js
@@ -15,3 +15,29 @@ window.onload = () => {
     initializePage();
     createMainContent();
 };
+const updatepicture = async (e) => {
+    e.preventDefault();
+    // let itemName = "Test"
+    // let newcatpicture = document.createElement('li')
+    // newcatpicture.innerHTML = itemName
+    // Catpic.appendChild(newcatpicture)
+        // Fetch image from API and set img url
+        try {
+            const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
+            // Converts to JSON
+            const kittenData = await kittenResponse.json();
+            // console.log(kittenData);
+            const kittenImg = document.querySelector("img");
+            kittenImg.src = kittenData[0].url;
+        } catch (e) {
+            console.log("Failed to fetch image", e);
+        }
+    };
+
+
+
+window.addEventListener("DOMContentLoaded", event => {
+    const changePicBtn = document.getElementById('picturechange')
+    changePicBtn.addEventListener('click', updatepicture)
+
+})
